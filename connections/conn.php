bf4b1bb5df0c9e073967ea301112aac3765bfe43
chicoizy <35073349+chicoizy@users.noreply.github.com>
2019-03-12 01:36:14 +0000

    Update conn.php

--- connections/conn.php
+++ connections/conn.php
@@ -1,8 +1,22 @@
 <?php
-
-$mysqli = new mysqli("servidordabasedados.database.windows.net", "chicoizy", "_Alface1", "basededadosdaloja");
-if ($mysqli->connect_errno) {
-	echo "Failed to connect sql: (" . $mysqli->connect_errno . ") ". $mysqli->connect_error;
-}
-
+    $serverName = "servidordabasedados.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "basededadosdaloja", // update me
+        "Uid" => "chicoizy", // update me
+        "PWD" => "_Alface1" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
