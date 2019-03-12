73a416769282b9d9f25371902b75fd861abe08ef
chicoizy <35073349+chicoizy@users.noreply.github.com>
2019-03-12 01:32:07 +0000

    Update conn.php

--- connections/conn.php
+++ connections/conn.php
@@ -1,16 +1,8 @@
 <?php
 
-//Declaracao de variaveis de ligacao
-$servername = "azuresqldb12.database.windows.net";
-$username = "chicoizy";
-$password = "_Alface1";
-
-$conn = mysqli_connect($servername,$username, $password);
-//Verificar a ligacao
-if(!$conn){
-	die("Erro de Ligação: ".mysqli_connect_error());
+$mysqli = new mysqli("servidordabasedados.database.windows.net", "chicoizy", "_Alface1", "basededadosdaloja");
+if ($mysqli->connect_errno) {
+	echo "Failed to connect sql: (" . $mysqli->connect_errno . ") ". $mysqli->connect_error;
 }
-mysqli_select_db($conn,"demo");
-mysqli_set_charset($conn,"utf8");
 
 ?>
